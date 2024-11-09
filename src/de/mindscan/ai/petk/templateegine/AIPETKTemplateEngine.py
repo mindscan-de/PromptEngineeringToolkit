27eae9ac58ffb68785136d61687359688ccb83cc
mindscan-de <maxim.gansert@gmail.com>
2024-11-09 19:48:44 +0100

    add translated python template engine.

---
+++ src/de/mindscan/ai/petk/templateegine/AIPETKTemplateEngine.py
@@ -0,0 +1,110 @@
+'''
+Created on 04.10.2024
+
+@author: JohnDoe
+'''
+
+import re
+
+class AIPETKTemplateEngine(object):
+    '''
+    classdocs
+    '''
+    FUN_QUOTEASJSON_STRING = "quoteAsJsonString"
+    FUN_TO_FIRST_UPPER = "toFirstUpper"
+
+    REPLACEMENT_IF_FUNCTION_UNKNOWN = 'UNKNOWN_FUNCTION'
+    REPLACEMENT_IF_KEY_UNKNOWN = 'UNKNOWN_KEY'
+    
+    toHEX = ['0','1','2','3', #
+             '4','5','6','7', #
+             '8','9','A','B', #
+             'C','D','E','F']
+    
+    selector_pattern = re.compile(r'\{{3}#([\w._:\[\]()]+)\}{3}')
+    
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def evaluateTemplate(self, template, value_map = None):
+        if not value_map:
+            value_map = {}
+            
+        result = ''
+        
+        selector_matcher = self.selector_pattern.finditer(template)
+        
+        last_end = 0
+        for match in selector_matcher:
+            result += template[last_end : match.start()]
+            result += self.evaluate_selector(match.groups()[0], value_map)
+            last_end = match.end()
+            
+        result += template[last_end:]
+        
+        return result        
+        
+        pass
+    
+    def evaluateSelector(self, selector, value_map):
+        if ':' in selector:
+            function_selector = selector.split(':', 2)
+            switch = {
+                AIPETKTemplateEngine.FUN_QUOTE_AS_JSON_STRING: lambda: self.json_string_quote(self.get_key_from_map(function_selector[1].strip(), value_map)),
+                AIPETKTemplateEngine.FUN_TO_FIRST_UPPER: lambda: self.to_first_upper(self.get_key_from_map(function_selector[1].strip(), value_map).strip()),
+            }
+            func = switch.get(function_selector[0], lambda: self.REPLACEMENT_IF_FUNCTION_UNKNOWN)
+            return func()
+        else:
+            return self.get_key_from_map(selector, value_map)
+        
+    @staticmethod
+    def to_first_upper(value):
+        if len(value) >= 2:
+            return value[0].upper() + value[1:]
+        return value.upper()
+
+    @staticmethod
+    def get_key_from_map(selector, value_map):
+        return value_map.get(selector, AIPETKTemplateEngine.REPLACEMENT_IF_KEY_UNKNOWN)
+
+    @staticmethod
+    def json_string_quote(value_to_escape):
+        if not value_to_escape:
+            return None
+        
+        builder = []
+        
+        for c in value_to_escape:
+            if c == '\\' or c == '"':
+                builder.extend(['\\', c])
+            elif ord(c) >= 0 and ord(c) <= 0x1f:
+                builder.append('\\')
+                if c == '\n':
+                    builder.append('n')
+                elif c == '\r':
+                    builder.append('r')
+                elif c == '\t':
+                    builder.append('t')
+                elif c == '\f':
+                    builder.append('f')
+                elif c == '\b':
+                    builder.append('b')
+                else:
+                    builder.extend(["u00", AIPETKTemplateEngine.toHEX[(ord(c) >> 4) & 0x1],
+                                   AIPETKTemplateEngine.toHEX[(ord(c)) & 0xF]])
+            elif ord(c) >= 0xD800 and ord(c) <= 0xDFFF:
+                builder.append("\\u")
+                builder.extend([AIPETKTemplateEngine.toHEX[(ord(c) >> 12) & 0xf],
+                               AIPETKTemplateEngine.toHEX[(ord(c) >> 8) & 0xf],
+                               AIPETKTemplateEngine.toHEX[(ord(c) >> 4) & 0xf],
+                               AIPETKTemplateEngine.toHEX[(ord(c)) & 0xf]])
+            else:
+                builder.append(c)
+                
+        return "\"" + ''.join(builder) + "\""
+            
