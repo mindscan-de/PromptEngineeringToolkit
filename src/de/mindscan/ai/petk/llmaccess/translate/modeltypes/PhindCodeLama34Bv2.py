b0e33d35b8e642c490a8d9380b76cef4806cc335
mindscan-de <maxim.gansert@gmail.com>
2024-11-23 16:37:01 +0100

    implement first Modeltype (as type hierarchy) instead of configuration

---
+++ src/de/mindscan/ai/petk/llmaccess/translate/modeltypes/PhindCodeLama34Bv2.py
@@ -0,0 +1,41 @@
+'''
+Created on 23.11.2024
+
+@author: JohnDoe
+'''
+from de.mindscan.ai.petk.llmaccess.translate.ModelType import ModelType
+
+class PhindCodeLama34Bv2(ModelType):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+    def get_qa_prompt_template_with_context(self):
+        return \
+            "\n" + \
+            "<<SYS>> {{{#system.prompt}}} <</SYS>>\n" + \
+            "Question: {{{#query}}}\n" +  \
+            "Context: [begin of context] {{{#context}}} [end of context]\n" + \
+            "Answer: "
+
+
+    def get_qa_prompt_template_with_context_and_pretext(self):
+        return \
+            "\n" + \
+            "<<SYS>> {{{#system.prompt}}} <</SYS>>\n" + \
+            "Question: {{{#query}}}\n" + \
+            "Context: [begin of context] {{{#context}}} [end of context]\n" + \
+            "Answer: {{{#pretext}}}"
+            
+    def get_simple_code_completion_template(self):
+        return "```{{{#language}}}\n{{{#context}}}"
+    
+    def get_raw_template(self):
+        return "{{{#context}}}"
+        
+        
